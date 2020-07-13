295692dee90d14e446b3d0cf8f6d0e72db9ec1f1
hanappe <hanappe@csl.sony.fr>
2020-07-13 22:49:07 +0100

    range for grbl; using rprintf

--- romi-rover/webproxy/webproxy.c
+++ romi-rover/webproxy/webproxy.c
@@ -62,10 +62,10 @@ static int set_server_dir(const char *path)
                         r_err("getcwd failed: %s", reason);
                         return -1;
                 }
-                snprintf(initial_path, PATH_MAX, "%s/%s", cwd, path);
+                rprintf(initial_path, PATH_MAX, "%s/%s", cwd, path);
                 initial_path[PATH_MAX-1] = 0;
         } else {
-                snprintf(initial_path, PATH_MAX, "%s", path);
+                rprintf(initial_path, PATH_MAX, "%s", path);
                 initial_path[PATH_MAX-1] = 0;
         }
 
@@ -86,7 +86,7 @@ static int set_server_dir(const char *path)
                 r_err("Not a directory: %s", resolved_path);
                 return -1;
         }
-        
+
         server_dir = r_strdup(resolved_path);
         r_info("Serving files from directory %s", server_dir);
         return 0;
@@ -96,7 +96,7 @@ static int get_configuration()
 {
         if (server_dir != NULL)
                 return 0;
-        
+
         r_debug("trying to configure the interface");
 
         json_object_t global_env = client_get("configuration", "");
@@ -104,7 +104,7 @@ static int get_configuration()
                 r_err("failed to load the configuration");
                 return -1;
         }
-        
+
         json_object_t config = json_object_get(global_env, "proxy");
 
         if (!json_isobject(config)) {
@@ -112,7 +112,7 @@ static int get_configuration()
                 json_unref(global_env);
                 return -1;
         }
-        
+
         const char *html = json_object_getstr(config, "html");
         if (html == NULL) {
                 r_err("invalid configuration");
@@ -124,7 +124,7 @@ static int get_configuration()
                 json_unref(global_env);
                 return -1;
         }
-                        
+
         json_unref(global_env);
 
         return 0;
@@ -134,10 +134,10 @@ static int init()
 {
         if (interface_initialized)
                 return 0;
-        
+
         if (get_configuration() != 0)
                 return -1;
-        
+
         interface_initialized = 1;
         return 0;
 }
@@ -147,16 +147,16 @@ static int check_path(const char *filename, char *path, int len, response_t *res
         char requested_path[PATH_MAX];
         char resolved_path[PATH_MAX];
         struct stat statbuf;
-        
+
         snprintf(requested_path, PATH_MAX, "%s/%s", server_dir, filename);
         requested_path[PATH_MAX-1] = 0;
 
         if (realpath(requested_path, resolved_path) == NULL) {
                 char reason[200];
                 strerror_r(errno, reason, 200);
-                if (errno == EACCES) 
+                if (errno == EACCES)
                         response_set_status(response, HTTP_Status_Forbidden);
-                else if (errno == ENOENT || errno == ENOTDIR) 
+                else if (errno == ENOENT || errno == ENOTDIR)
                         response_set_status(response, HTTP_Status_Not_Found);
                 else
                         response_set_status(response, HTTP_Status_Internal_Server_Error);
@@ -172,9 +172,9 @@ static int check_path(const char *filename, char *path, int len, response_t *res
         if (stat(resolved_path, &statbuf) != 0) {
                 char reason[200];
                 strerror_r(errno, reason, 200);
-                if (errno == EACCES) 
+                if (errno == EACCES)
                         response_set_status(response, HTTP_Status_Forbidden);
-                else if (errno == ENOENT || errno == ENOTDIR) 
+                else if (errno == ENOENT || errno == ENOTDIR)
                         response_set_status(response, HTTP_Status_Not_Found);
                 else
                         response_set_status(response, HTTP_Status_Internal_Server_Error);
@@ -223,12 +223,12 @@ static void send_file(const char *path, const char *mimetype,
 int webproxy_init(int argc, char **argv)
 {
         r_log_set_writer(broadcast_log, NULL);
-        
+
         if (argc == 2) {
                 if (set_server_dir(argv[1]) != 0)
                         return -1;
         }
-        
+
         for (int i = 0; i < 10; i++) {
                 if (init() == 0)
                         return 0;
@@ -236,7 +236,7 @@ int webproxy_init(int argc, char **argv)
                 clock_sleep(0.2);
         }
 
-        r_err("failed to initialize the interface");        
+        r_err("failed to initialize the interface");
         return -1;
 }
 
@@ -253,7 +253,7 @@ void send_local_file(const char *filename, request_t *request, response_t *respo
         char path[PATH_MAX];
 
         r_info("send_local_file: %s", filename);
-        
+
         const char *mimetype = filename_to_mimetype(filename);
 
         if (check_path(filename, path, PATH_MAX, response) == 0)
@@ -278,12 +278,12 @@ void webproxy_send_file(void *data, request_t *request, response_t *response)
 
 	const char *filename = request_uri(request);
         r_debug("uri: %s", request_uri(request));
-        
+
         if (rstreq(filename, "/"))
                 filename = "index.html";
         else if (filename[0] == '/')
                 filename++;
-        
+
         send_local_file(filename, request, response);
 }
 
@@ -314,16 +314,16 @@ void webproxy_redirect(void *data, request_t *request, response_t *response)
         // Expecting the following pattern: /<type>/<topic>/<dir1>/.../<file>
         // The elements list should contain the following strings:
         //  "/"->"<type>"->"<topic>"->"<dir1>"->...->"<file>"
-        
+
         if (list_size(elements) >= 4) {
-        
+
                 char resource[1024];
                 const char *slash = list_nth_string(elements, 0);
                 const char *type = list_nth_string(elements, 1);
                 const char *topic = list_nth_string(elements, 2);
                 int err = make_uri(resource, sizeof(resource),
                                    request, list_nth(elements, 3));
-                
+
                 if ((slash == NULL)
                     || !rstreq(slash, "/")
                     || (type == NULL)
@@ -333,13 +333,13 @@ void webproxy_redirect(void *data, request_t *request, response_t *response)
                     || (strlen(resource) == 0)) {
                         response_set_status(response, HTTP_Status_Bad_Request);
                 } else {
-                        client_get_data(topic, resource, &response);        
+                        client_get_data(topic, resource, &response);
                 }
-                
+
         } else {
                 response_set_status(response, HTTP_Status_Bad_Request);
         }
-        
+
         path_delete(elements);
 }
 
@@ -396,7 +396,7 @@ void webproxy_onrequest(void *data,
                         response_t *response)
 {
 	const char *path = request_uri(request);
-        if (strncmp(path, "/service/", 9) == 0) 
+        if (strncmp(path, "/service/", 9) == 0)
                 webproxy_redirect(data, request, response);
         else
                 webproxy_send_file(data, request, response);
@@ -463,7 +463,7 @@ int webproxy_onconnect(void *userdata, messagehub_t *hub,
 {
         list_t* elements = path_break(request_uri(request));
         int err = -1;
-        
+
         // Expecting the following pattern: /messagehub/<topic>
         // The elements list should contain the following strings:
         //  "/" -> "messagehub" -> "<topic>"
@@ -472,21 +472,21 @@ int webproxy_onconnect(void *userdata, messagehub_t *hub,
                 const char *slash = list_nth_string(elements, 0);
                 const char *type = list_nth_string(elements, 1);
                 const char *topic = list_nth_string(elements, 2);
-                
+
                 if ((slash != NULL)
                     && rstreq(slash, "/")
                     && (type != NULL)
                     && rstreq(type, "messagehub")
                     && (topic != NULL)
                     && (strlen(topic) > 0)) {
-                        
+
                         err = webproxy_redirect_websocket(link_from_client, topic);
-                        
+
                 } else {
                         r_debug("webproxy_onconnect: Invalid path: %s",
                                 request_uri(request));
                 }
-                
+
         } else {
                 r_debug("webproxy_onconnect: Wrong path length: %s",
                         request_uri(request));
@@ -495,4 +495,3 @@ int webproxy_onconnect(void *userdata, messagehub_t *hub,
         path_delete(elements);
         return err;
 }
-
