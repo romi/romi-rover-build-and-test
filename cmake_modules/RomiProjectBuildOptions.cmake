01d62706f6686f146ad3b9d72e6b96d0d637cc9d
Doug Boari <doug@tastymushroom.com>
2021-03-05 15:12:44 +0000

    Add -Winit-self -Waddress -Wlogical-op -Wpointer-arith -Wformat=2 warnings.

--- cmake_modules/RomiProjectBuildOptions.cmake
+++ cmake_modules/RomiProjectBuildOptions.cmake
@@ -18,9 +18,9 @@ endif()
 
 # For now everything is debug. Set Compile options locally to maintain independent library builds.
 # Ideal list of warnings! Add gradually!
-#"-Wall -Wextra -Wpedantic -Werror -Wmissing-include-dirs -Wconversion -Wsign-conversion -Weffc++ -Wzero-as-null-pointer-constant -Wswitch-default -Wswitch-enum -Winit-self -Waddress -Wlogical-op -Wpointer-arith -Wformat=2)
+#"-Wall -Wextra -Wpedantic -Werror -Wmissing-include-dirs -Wconversion -Wsign-conversion -Weffc++ -Wuseless-cast -Wzero-as-null-pointer-constant -Wswitch-default -Wswitch-enum -Winit-self -Waddress -Wlogical-op -Wpointer-arith -Wformat=2)
 #set(COMMON_COMPILATION_FLAGS "-Wall -Wextra -Wpedantic -Werror -Wmissing-include-dirs -Wconversion -Wsign-conversion -Wzero-as-null-pointer-constant -Wswitch-default -Winit-self -Waddress -Wlogical-op -Wpointer-arith -Wformat=2")
-set(COMMON_COMPILATION_FLAGS "-Wall -Wextra -Wpedantic -Werror -Wmissing-include-dirs -Wconversion -Wsign-conversion -Wzero-as-null-pointer-constant -Wswitch-default")
+set(COMMON_COMPILATION_FLAGS "-Wall -Wextra -Wpedantic -Werror -Wmissing-include-dirs -Wconversion -Wsign-conversion -Wzero-as-null-pointer-constant -Wswitch-default -Winit-self -Waddress -Wlogical-op -Wpointer-arith -Wformat=2")
 set(CMAKE_BUILD_TYPE Debug)
 set(CMAKE_CXX_FLAGS "${COMMON_COMPILATION_FLAGS} -Wuseless-cast -Weffc++ ${PROJECT_SANITISE_FLAGS}")
 set(CMAKE_C_FLAGS "${COMMON_COMPILATION_FLAGS} ${PROJECT_SANITISE_FLAGS}")
