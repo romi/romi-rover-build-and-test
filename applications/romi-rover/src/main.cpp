4b2bb7c7ec44b18457377768db86af0904149967
hanappe <peter@hanappe.com>
2021-05-05 18:07:47 +0100

    improving info messages during boot

--- applications/romi-rover/src/main.cpp
+++ applications/romi-rover/src/main.cpp
@@ -116,6 +116,7 @@ int main(int argc, char** argv)
                 rpp::Linux linux;
 
                 // Session
+                r_info("main: Creating session");
                 romi::RomiDeviceData romiDeviceData;
                 romi::SoftwareVersion softwareVersion;
                 romi::Gps gps;
@@ -126,6 +127,7 @@ int main(int argc, char** argv)
                 session.start("hw_observation_id");
 
                 // Display
+                r_info("main: Creating display");
                 const char *display_device = (const char *) config["ports"]["display-device"]["port"];
                 
                 auto display_serial = romiserial::RomiSerialClient::create(display_device);
@@ -133,18 +135,21 @@ int main(int argc, char** argv)
                 display.show(0, "Initializing");
 
                 // Joystick
+                r_info("main: Creating joystick");
                 const char *joystick_device = (const char *) config["ports"]["joystick"]["port"];
                 romi::LinuxJoystick joystick(linux, joystick_device);
                 romi::UIEventMapper joystick_event_mapper;
                 romi::JoystickInputDevice input_device(joystick, joystick_event_mapper);
 
                 // CNC controller
+                r_info("main: Creating CNC controller");
                 const char *cnc_device = (const char *) config["ports"]["oquam"]["port"];
                 auto cnc_serial = romiserial::RomiSerialClient::create(cnc_device);
                 romi::StepperController cnc_controller(cnc_serial);
 
                 
                 // CNC
+                r_info("main: Creating CNC");
                 JsonCpp r = config["oquam"]["cnc-range"];
                 romi::CNCRange range(r);
                 JsonCpp s = config["oquam"]["stepper-settings"];
@@ -152,7 +157,8 @@ int main(int argc, char** argv)
                 double slice_duration = (double) config["oquam"]["path-slice-duration"];
                 double maximum_deviation = (double) config["oquam"]["path-maximum-deviation"];
 
-                romi::AxisIndex homing[3] = { romi::kAxisX, romi::kAxisY, romi::kNoAxis };
+                //romi::AxisIndex homing[3] = { romi::kAxisX, romi::kAxisY, romi::kNoAxis };
+                romi::AxisIndex homing[3] = { romi::kNoAxis, romi::kNoAxis, romi::kNoAxis };
                 romi::OquamSettings oquam_settings(range,
                                                    stepper_settings.maximum_speed,
                                                    stepper_settings.maximum_acceleration,
@@ -164,6 +170,7 @@ int main(int argc, char** argv)
 
                 // Camera
                 // TBD: Use refactored functions. get_camera_class
+                r_info("main: Creating camera");
                 std::unique_ptr<romi::ICamera> camera;
                 std::string camera_classname = get_camera_classname(options, config);
                 
@@ -185,15 +192,18 @@ int main(int argc, char** argv)
                 }
                 
                 // Weeder pipeline
+                r_info("main: Creating weeder pipeline");
                 romi::PipelineFactory pipeline_factory;
                 romi::IPipeline& pipeline = pipeline_factory.build(range, config);
 
                 // Weeder
+                r_info("main: Creating weeder");
                 double z0 = (double) config["weeder"]["z0"];
                 double speed = (double) config["weeder"]["speed"];
                 romi::Weeder weeder(*camera, pipeline, oquam, z0, speed, session);
 
-                // Navigation
+                // Motor driver
+                r_info("main: Creating motor driver");
                 JsonCpp rover_settings = config["navigation"]["rover"];
                 romi::NavigationSettings rover_config(rover_settings);
                 const char *driver_device = (const char *) config["ports"]["brush-motor-driver"]["port"];
@@ -202,26 +212,38 @@ int main(int argc, char** argv)
                 romi::BrushMotorDriver driver(driver_serial, driver_settings,
                                               static_cast<int>(rover_config.encoder_steps),
                                               rover_config.max_revolutions_per_sec);
+                
+                // Navigation
+                r_info("main: Creating navigation");
                 romi::Navigation navigation(driver, rover_config);
 
                 // SpeedController
+                r_info("main: Creating speed controller");
                 romi::SpeedController speed_controller(navigation, config);
 
                 // EventTimer
+                r_info("main: Creating event timer");
                 romi::EventTimer event_timer(romi::event_timer_timeout);
 
                 // Scripts and script engine
-                romi::ScriptList scripts(get_script_file(options, config));
+                std::string script_file = get_script_file(options, config);
+                r_info("main: Loading scripts from %s", script_file.c_str());
+                romi::ScriptList scripts(script_file);
+                
+                r_info("main: Creating scripts engine");
                 romi::ScriptMenu menu(scripts);
                 romi::RoverScriptEngine script_engine(scripts, romi::event_script_finished,
                                                       romi::event_script_error);
 
                 // Notifications
                 std::string soundfont = get_sound_font_file(options, config);
+                r_info("main: Loading soundfont %s", soundfont.c_str());
                 JsonCpp sound_setup = config["user-interface"]["fluid-sounds"]["sounds"];
+                r_info("main: Creating sound notifications");
                 romi::FluidSoundNotifications notifications(soundfont, sound_setup);
 
                 // Rover
+                r_info("main: Creating rover");
                 romi::Rover rover(input_device,
                             display,
                             speed_controller,
@@ -233,15 +255,20 @@ int main(int argc, char** argv)
                             weeder);
 
                 // State machine
+                r_info("main: Creating state machine");
                 romi::RoverStateMachine state_machine(rover);
 
                 // User interface
+                r_info("main: Creating user interface");
                 romi::RoverInterface user_interface(rover, state_machine);
 
+                r_info("main: Sending start event");
                 
                 if (!state_machine.handle_event(romi::event_start))
                         // FIXME: should not quit but display something
                         throw std::runtime_error("start-up failed");
+                
+                r_info("main: Starting event loop");
                 
                 while (!quit) {
                         
