33966e1afcc26419cff3338d941df9fab7da063f
Doug Boari <doug@tastymushroom.com>
2021-06-09 10:31:58 +0200

    Add scratch test application. Bare minimum app for testing different cases. Currently used for testing unet.

---
+++ applications/scratch-test/src/main.cpp
@@ -0,0 +1,170 @@
+/*
+  romi-rover
+
+  Copyright (C) 2019-2020 Sony Computer Science Laboratories
+  Author(s) Peter Hanappe
+
+  romi-rover is collection of applications for the Romi Rover.
+
+  romi-rover is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+
+ */
+#include <exception>
+#include <stdexcept>
+#include <memory>
+#include <atomic>
+#include <syslog.h>
+#include <string.h>
+#include <thread>
+
+#include <RSerial.h>
+#include <RomiSerialClient.h>
+#include <Linux.h>
+#include <Clock.h>
+#include <ClockAccessor.h>
+#include <configuration/ConfigurationProvider.h>
+#include <rover/Rover.h>
+#include <rover/RoverOptions.h>
+#include <rover/RoverInterface.h>
+#include <rover/RoverScriptEngine.h>
+#include <rover/RoverStateMachine.h>
+#include <rover/SpeedController.h>
+#include <api/EventTimer.h>
+#include <ui/ScriptList.h>
+#include <ui/ScriptMenu.h>
+#include <notifications/FluidSoundNotifications.h>
+#include <rover/Navigation.h>
+#include <ui/CrystalDisplay.h>
+#include <ui/JoystickInputDevice.h>
+#include <weeder/Weeder.h>
+#include <api/IDisplay.h>
+#include <ui/LinuxJoystick.h>
+#include <ui/UIEventMapper.h>
+#include <ui/CrystalDisplay.h>
+#include <ui/JoystickInputDevice.h>
+#include <camera/FileCamera.h>
+#include <camera/USBCamera.h>
+#include <rpc/RemoteCamera.h>
+#include <rpc/RcomClient.h>
+#include <hal/BrushMotorDriver.h>
+#include <oquam/StepperController.h>
+#include <oquam/StepperSettings.h>
+#include <oquam/Oquam.h>
+#include <weeder/PipelineFactory.h>
+#include <camera/Imager.h>
+#include <unet/UnetImager.h>
+#include <data_provider/RomiDeviceData.h>
+#include <data_provider/SoftwareVersion.h>
+#include <session/Session.h>
+#include <data_provider/Gps.h>
+#include <data_provider/GpsLocationProvider.h>
+#include <RegistryServer.h>
+
+std::atomic<bool> quit(false);
+
+void SignalHandler(int signal)
+{
+        if (signal == SIGSEGV){
+                syslog(1, "rcom-registry segmentation fault");
+                exit(signal);
+        }
+        else if (signal == SIGINT){
+                r_info("Ctrl-C Quitting Application");
+                perror("init_signal_handler");
+                quit = true;
+        }
+        else{
+                r_err("Unknown signam received %d", signal);
+        }
+}
+
+int main(int argc, char** argv)
+{
+        std::shared_ptr<rpp::IClock> clock = std::make_shared<rpp::Clock>();
+        rpp::ClockAccessor::SetInstance(clock);
+
+        int retval = 1;
+
+        romi::RoverOptions options;
+        options.parse(argc, argv);
+        options.exit_if_help_requested();
+
+        r_log_init();
+        r_log_set_app("romi-rover");
+
+        std::signal(SIGSEGV, SignalHandler);
+        std::signal(SIGINT, SignalHandler);
+
+        try {
+
+                rpp::Linux linux;
+
+                // Session
+                r_info("main: Creating session");
+                romi::RomiDeviceData romiDeviceData;
+                romi::SoftwareVersion softwareVersion;
+                romi::Gps gps;
+                std::unique_ptr<romi::ILocationProvider> locationProvider
+                        = std::make_unique<romi::GpsLocationProvider>(gps);
+                std::string session_directory =".";
+
+                romi::Session session(linux, session_directory, romiDeviceData,
+                                      softwareVersion, std::move(locationProvider));
+//                session.start("hw_observation_id");
+
+//            std::string registry = "192.168.0.179";
+//            if (!registry.empty())
+                rcom::RegistryServer::set_address("192.168.1.22");
+
+                romi::PythonUnet pythonUnet;
+                std::string image_file("./camera.jpg");
+                std::unique_ptr<romi::FileCamera> camera = std::make_unique<romi::FileCamera>(image_file);
+
+                // Imager
+                //romi::Imager imager(session, *camera);
+                std::string observation_id("observe");
+                romi::UnetImager imager(session, *camera);
+                imager.start_recording(observation_id, 500, 500.00);
+
+                while (!quit) {
+                        
+                        try {
+                            using namespace std::chrono_literals;
+                            std::this_thread::sleep_for(500ms);
+                            if (!imager.is_recording())
+                            {
+                                std::this_thread::sleep_for(500ms);
+//                                quit = true;
+                                r_info("Recording stopped Quitting Application");
+                            }
+                        } catch (std::exception& e) {
+                            quit = true;
+                                throw;
+                        }
+                }
+                imager.stop_recording();
+
+                retval = 0;
+
+                
+        } catch (JSONError& je) {
+                r_err("main: Invalid configuration file: %s", je.what());
+                
+        } catch (std::exception& e) {
+                r_err("main: exception: %s", e.what());
+        }
+        
+        return retval;
+}
