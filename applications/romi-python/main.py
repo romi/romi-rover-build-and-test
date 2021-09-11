870dbbcdb254253d848e1c2145b5ed3348e25355
hanappe <hanappe@csl.sony.fr>
2021-09-11 19:52:39 +0200

    update

--- applications/romi-python/main.py
+++ applications/romi-python/main.py
@@ -2,7 +2,7 @@ import asyncio
 import argparse
 from romi.rpc import Server
 
-from unet import unet_init, unet_handle_request
+#from unet import unet_init, unet_handle_request
 #from svm import svm_init, svm_handle_request
 from svm0 import svm0_init, svm0_handle_request
 from nav import nav_init, nav_handle_request
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     print("Starting server")
     server = Server("python",
                     {
-                        "unet": unet_handle_request,
+                        #"unet": unet_handle_request,
                         #"svm": svm_handle_request,
                         "svm": svm0_handle_request,
                         "nav": nav_handle_request
