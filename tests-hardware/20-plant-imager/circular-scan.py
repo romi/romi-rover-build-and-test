7f09fe67d1b523f91e25465ef8f12419b2a90913
hanappe <hanappe@csl.sony.fr>
2022-09-22 17:04:37 +0200

    update plant imager

--- tests-hardware/20-plant-imager/circular-scan.py
+++ tests-hardware/20-plant-imager/circular-scan.py
@@ -2,25 +2,68 @@ import argparse
 import math
 import time
 import json
+import os
+from datetime import datetime
 from romi.remote_device import OquamXYTheta
 from romi.camera import Camera
+
+def new_session_id():
+    return datetime.now().strftime("%Y%m%d-%H%M%S")
+
+def create_session(db, session_id):
+    path = os.path.join(db, session_id)
+    os.makedirs(path)
+    path = os.path.join(db, session_id, "metadata")
+    os.makedirs(path)
+    
+def create_fileset(db, session_id, fileset_id):
+    path = os.path.join(db, session_id, fileset_id)
+    os.makedirs(path)
         
+def store_image(db, session_id, fileset_id, name, image):
+    path = os.path.join(db, session_id, fileset_id, name)
+    image.save(path)
+
+def store_metadata(db, session_id, fileset_id, metadata):
+    path = os.path.join(db, session_id, "metadata", f"{fileset_id}.json")
+    with open(path, 'w') as outfile:
+        json.dump(metadata, outfile, indent=4)
+
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--registry', type=str, nargs='?', default="127.0.0.1",
+    parser.add_argument('--registry', type=str, default="127.0.0.1",
                     help='IP address of the registry')
+    parser.add_argument('--count', type=int, default=36,
+                    help='The number of images')
+    parser.add_argument('--database', type=str, default=".",
+                        help='The path to the database (WORK IN PROGRESS)')
     args = parser.parse_args()
     
     cnc = OquamXYTheta("cnc", args.registry)
     cnc.power_up()
     cnc.homing()
 
-    camera1 = Camera("camera-top", args.registry)
-    #camera2 = Camera("camera-bottom", args.registry)
-    camera1.select_option('exposure-mode', 'off')
-    camera1.set_value('analog-gain', 10)
-    camera1.set_value('shutter-speed', 60000)
-    #camera1.set_value('jpeg-quality', 95)
+    camera_names = ["camera-top"]
+    cameras = []
+    
+    for name in camera_names:
+        camera = Camera(name, args.registry)
+        cameras.append(Camera(name, args.registry))
+        # TODO: read from settings?
+        camera.select_option('exposure-mode', 'off')
+        camera.set_value('analog-gain', 10)
+        camera.set_value('shutter-speed', 60000)
+        #camera.set_value('jpeg-quality', 95)
+
+
+
+    db = args.database
+    session_id = new_session_id()
+    fileset_id = "images"
+    create_session(db, session_id)
+    create_fileset(db, session_id, fileset_id)
+
     
     dimension = cnc.get_range()
     print(f"dimension={dimension}")
@@ -33,27 +76,28 @@ if __name__ == '__main__':
     print(f"c=({xc}, {yc}), r={r}")
 
     speed = 0.8
-    N = 16
+    N = args.count
+    angle = -2.0 * math.pi / N
     
     cnc.moveto(xc - r, yc, 0.0, speed)
 
-    metadata = {}
-    
-    for i in range(N):
-        angle = -2.0 * math.pi / N
-        cnc.helix(xc, yc, angle, angle, speed)
-        image1 = camera1.grab()
-        name1 = f"camera1-{i:05d}.jpg"
-        image1.save(name1)
-        position = cnc.get_position()
-        metadata[name1] = {'camera': name1,
-                           'camera-index': 0,
-                           'index': i,
-                           'position': {'x': position['x'],
-                                        'y': position['y'],
-                                        'pan': position['z']}}
+    metadata = { 'cameras': camera_names }
 
-    with open('metadata.json', 'w') as outfile:
-        json.dump(metadata, outfile, indent=4)
+    for i in range(N):
+        for j in range(len(cameras)):
+            camera_name = camera_names[j]
+            camera = cameras[j]
+            image = camera.grab()
+            name = f"{camera_name}-{i:05d}.jpg"
+            store_image(db, session_id, fileset_id, name, image)
+            position = cnc.get_position()
+            metadata[name] = {'camera': camera_name,
+                              'index': i,
+                              'position': {'x': position['x'],
+                                           'y': position['y'],
+                                           'pan': position['z']}}
+            cnc.helix(xc, yc, angle, angle, speed)
+
+    store_metadata(db, session_id, fileset_id, metadata)
         
     cnc.moveto(0, 0, 0, speed)
