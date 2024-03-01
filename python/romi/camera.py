9298dce0af5cb575bd024d1356101c24c38f90f8
hanappe <peter@hanappe.com>
2024-03-01 16:40:38 +0100

    Added python fake camera

--- python/romi/camera.py
+++ python/romi/camera.py
@@ -41,13 +41,47 @@ class Camera(RcomClient):
         self.execute('power-down')
 
         
+
+class FakeCamera(RcomClient):
+
+    
+    def __init__(self, topic = 'camera', id = 'camera', file = 'test.jpg'):
+        super().__init__(topic, id)
+        self.image = Image.open(file)
+        
+    def print_error(self, data):
+       print(f"Failed to grab the image: {r['error']['message']}")
+       
+    def grab(self):
+        return self.image
+
+    def set_value(self, name, value):
+        params = {'name': name, 'value': value}
+        print(f'camera:set-value: {name}={value}')
+
+    def select_option(self, name, value):
+        params = {'name': name, 'value': value}
+        print(f'camera:select-option: {name}={value}')
+       
+    def power_up(self):
+        #print(f'power-up')
+        pass
+        
+    def power_down(self):
+        #print(f'power-down')
+        pass
+
+        
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--topic', type=str, nargs='?', default="camera",
-                    help='The regsitry topic')
+                    help='The registry topic')
+    parser.add_argument('--file', type=str, nargs='?', default="test.jpg",
+                    help='The file for the fake camera')
     args = parser.parse_args()
     
-    camera = Camera(args.topic, args.topic)
+    #camera = Camera(args.topic, args.topic)
+    camera = FakeCamera(args.topic, args.topic, args.file)
     for i in range(10):
         image = camera.grab()
         if image != None:
